cae2dbd695417535372b13a56164450c89d09e01
Krishnanathwani <50243538+Krishnanathwani@users.noreply.github.com>
2020-06-27 00:05:18 +0530

    My first commit
    initial commit

---
+++ js/main.js
@@ -0,0 +1,38 @@
+//Google Maps API
+
+// Initialize and add the map
+function initMap() {
+  // Your location
+  const loc = { lat: 17.370766, lng: 78.421052 };
+  // Centered map on location
+  const map = new google.maps.Map(document.querySelector('#map'), {
+    zoom: 14,
+    center: loc
+  });
+  // The marker, positioned at location
+  const marker = new google.maps.Marker({ position: loc, map: map });
+}
+//smooth scrolling
+$('#navbar a, .btn').on('click', function(event) {
+  if (this.hash !== '') {
+    event.preventDefault();
+
+    const hash = this.hash;
+
+    $('html, body').animate(
+      {
+        scrollTop: $(hash).offset().top - 100
+      },
+      800
+    );
+  }
+});
+
+//transparent navbar
+window.addEventListener('scroll', function() {
+  if (window.scrollY > 150) {
+    document.querySelector('#navbar').style.opacity = 0.9;
+  } else {
+    document.querySelector('#navbar').style.opacity = 1;
+  }
+});
